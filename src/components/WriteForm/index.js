a719c18949ba1f9be6afa10f8cd56ca1e6d23a41
dahye <song.dahye@nhn.com>
2019-03-21 19:25:01 +0900

    step5 state component create - 버튼누르면인풋데이터가져와서출력하기

--- src/components/WriteForm/index.js
+++ src/components/WriteForm/index.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react'
 import './WriteForm.scss'
 
 class WriteForm extends Component {
+  inputRef = React.createRef()
+
+  handleSubmit = () => {
+    const { onCreate } = this.props
+    const text = this.inputRef.current.value
+    onCreate(text)
+  }
+
   render () {
+    
     return (
       <div className='write_form_area'>
         <label htmlFor='todoText' className='blind'>할 일</label>
-        <input type='text' className='input_text' placeholder='할 일을 적어주세요' />
-        <button type='button' className='btn'>등록</button>
+        <input 
+          type='text' 
+          className='input_text' 
+          placeholder='할 일을 적어주세요' 
+          ref={this.inputRef}
+        />
+        <button type='button' className='btn' onClick={this.handleSubmit}>등록</button>
       </div>
     )
   }
--- src/pages/TodoListPage/index.js
+++ src/pages/TodoListPage/index.js
@@ -5,13 +5,19 @@ import Footer from '../../components/Footer'
 import WriteForm from '../../components/WriteForm'
 import TodoList from '../../components/TodoList'
 
+// WriteForm에서 버튼을 눌렀을 때 인풋의 value값을 가져와서 TodoList에 뿌려주기
+
 class TodoListPage extends Component {
+  handleCreate = (text) => {
+    console.log('버튼을 누르면 실행되는 함수', text)
+  }
+
   render () {
     return (
       <div>
         <Header />
         <div className='content'>
-          <WriteForm />
+          <WriteForm onCreate={this.handleCreate}/>
           <TodoList />
         </div>
         <Footer />
