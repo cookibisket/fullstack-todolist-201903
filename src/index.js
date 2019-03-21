73394791d23e7017ce8d818550c528e978c3767e
dahye <song.dahye@nhn.com>
2019-03-21 16:26:51 +0900

    step3 node-sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
+import './assets/scss/common.scss'
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
