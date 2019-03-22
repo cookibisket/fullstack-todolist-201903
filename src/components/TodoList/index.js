42b144ed47488cabb2e444198fd2edb358991d68
dahye <song.dahye@nhn.com>
2019-03-22 15:50:59 +0900

    step5 state component create - 맨처음 입력값 없을때 안내문구 노출시키기

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -4,15 +4,19 @@ import './TodoList.scss'
 const TodoList = ({ todos }) => {
   return (
     <div className='todolist_area'>
-      <ul className='list_todolist'>
-        {todos.map(todo => {
-          return (
-            <li className='list_item' key={todo.id}>
-              <div className='title'>{todo.text}</div>
-            </li>
-          )
-        })}
-      </ul>
+      {todos.length === 0 ? (
+        <div className="nodata_area">등록된 할 일이 없습니다.</div>
+      ) : (
+        <ul className='list_todolist'>
+          {todos.map(todo => {
+            return (
+              <li className='list_item' key={todo.id}>
+                <div className='title'>{todo.text}</div>
+              </li>
+            )
+          })}
+        </ul>
+      )}
     </div>
   )
 }
