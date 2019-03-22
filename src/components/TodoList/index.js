b0a738e5852d4c65f8786c96cb7cb638a3c0185e
dahye <song.dahye@nhn.com>
2019-03-22 15:33:26 +0900

    step5 state component create - 배열을 map으로 돌려 출력하기

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -5,9 +5,13 @@ const TodoList = ({ todos }) => {
   return (
     <div className='todolist_area'>
       <ul className='list_todolist'>
-        <li className='list_item'>
-          <div className='title'>{todos}</div>
-        </li>
+        {todos.map(todos => {
+          return (
+            <li className='list_item'>
+              <div className='title'>{todos}</div>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
